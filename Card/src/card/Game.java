bcb5ebe6472c0c81a63c482c3e433eb2170f4db0
Matt <matt.johnston@uvm.edu>
2015-04-18 15:21:32 -0400

    adding new go

--- Card/src/card/Game.java
+++ Card/src/card/Game.java
@@ -10,10 +10,11 @@ public class Game extends CardObject {
     static protected String endCondition, difficulty, name;
     static protected DiscardPile dp = new DiscardPile();
     static protected GuiClassTest gui = new GuiClassTest();
-    static public int roundCount = 0, counter = 0;
-    static public boolean gameState = true, userTurnDone = false,aiTurnDone = false;
+    static public int roundCount = 0, counter = 0, index = 0;
+    static public boolean gameState = true, userTurnDone, op1TurnDone,
+            op2TurnDone, op3TurnDone, action, turnsDone = false;
     static Card dpTop;
-    static Hand playerHand,aiHand;
+    static Hand playerHand, aiHand;
 
     public Game(String endCondition, int numOfAI, String difficulty, String name) {
         //creates a new deck
@@ -26,12 +27,15 @@ public class Game extends CardObject {
         this.name = name;
 
         //sets the easy ai
-        ai = new EasyAI(numOfAI);
+        ai = new EasyAI(numOfAI+1);
 
         //deals hands to players
-        for (int i = 0; i < numOfAI; i++) {
+        for (int i = 0; i < numOfAI + 1; i++) {
             hands.add(new Hand(deck.pop(), deck.pop(), deck.pop(), deck.pop()));
         }
+        //creates the user hand
+        playerHand = new Hand(hands.get(0).peek(0), hands.get(0).peek(1), hands.get(0).peek(2), hands.get(0).peek(3));
+        
         //pops a card from the deck
         dpTop = deck.pop();
 
@@ -46,53 +50,66 @@ public class Game extends CardObject {
 
         //creates gui card layout
         gui.createCards(numOfAI, dp);
-        run(true, false,false);
+        run(true, false, false, false, false);
 
     }// End of Game constructor
 
-    public static void run(boolean gameState, boolean userTurnDone,boolean aiTurnDone) {
-        Game.userTurnDone = userTurnDone;
-        Game.gameState = gameState;
+    public static void run(boolean gameState, boolean userTurnDone, boolean op1TurnDone,
+            boolean op2TurnDone, boolean op3TurnDone) {
+
         // GAME LOOP
+        //end game options can go in the while loop
         while (gameState && roundCount < 10) {
             System.out.println("Round # : " + roundCount);
             for (int i = 0; i < hands.size(); i++) {
                 // Player's Turn
-                if (i == 0 && userTurnDone != true) {
+                if (i == 0 && userTurnDone != true && op1TurnDone == false
+                        && op2TurnDone == false && op3TurnDone == false) {
                     gameState = false;
-                    printStatus();
-                } //AI's Turn
-                if(aiTurnDone != true){
-                    //gets AI hand
-                    aiHand = new Hand(hands.get(i).peek(0),hands.get(i).peek(1),hands.get(i).peek(2),hands.get(i).peek(3));
-                    //if discard pile is not empty
-                    if (!dp.discardPile.isEmpty()) {
-                        Card dpPeek = dp.draw(); //draws from discard pile
-                        boolean action = ai.DrawOrDiscard(dpPeek); //gets decision
-                        gui.opponentTurn(aiHand,action,i); //method for the ai to make a move
-                        gameState = false;
-                    } else {
-                        boolean action = ai.DrawOrDiscard(); //gets decision
-                        gui.opponentTurn(aiHand,action,i); //method for the ai to make a move
-                        gameState = false;
-                    }
+                    //passes the hand and the round number to the gui
+                    gui.UserTurn(playerHand, roundCount);
                 }
+                if (i > 0 && userTurnDone == true) {
+                    //loop thru opponents hands
+                    for (int op = 1; op < hands.size(); op++) {
+                        //waits for the user input
+                            //gets AI hand
+                            aiHand = new Hand(hands.get(op).peek(0), hands.get(op).peek(1), hands.get(op).peek(2), hands.get(op).peek(3));
+                            if (op1TurnDone != true && op == 1) {
+                                //if discard pile is not empty
+                                if (!dp.discardPile.isEmpty()) {
+                                    Card dpPeek = dp.draw(); //draws from discard pile
+                                    action = ai.DrawOrDiscard(dpPeek); //returns action
+                                } else {
+                                    action = ai.DrawOrDiscard(); //returns true if discardpile is empty
+                                }
+                                gui.opponentTurn(aiHand, action, op); //method for the ai to make a move
+                            }
+                            if (op2TurnDone != true && op == 2) {
+                                op2TurnDone = true;
+                                gui.opponentTurn(aiHand, action, op); //method for the ai to make a move
+                            }
+                            if (op3TurnDone != true && op == 3) {
+                                op3TurnDone = true;
+                                gui.opponentTurn(aiHand, action, op); //method for the ai to make a move
+                            }
+                        //userTurnDone = false;
+                    }//end of inner for loop
+                    userTurnDone = false;
+                    op1TurnDone = false;
+                    op2TurnDone = false;
+                    op3TurnDone = false;
+                    gameState = true;
+                    turnsDone = true;
+                }//end if
+
+            }//end of outer for loop
+            if (turnsDone == true) {
+                roundCount++;
+                turnsDone = false;
             }
-            Game.userTurnDone = false;
-            Game.aiTurnDone = false;
-            Game.gameState = true;
-            roundCount++;
         }
-        Game.gameState = true;
 
     }// End of run()
-
-    static protected void printStatus() {
-        //creates the user hand
-        playerHand = new Hand(hands.get(0).peek(0), hands.get(0).peek(1), hands.get(0).peek(2), hands.get(0).peek(3));
-        //passes the hand and the round number to the gui
-        gui.UserTurn(playerHand, roundCount);
-    }
-
 
 }// End of Game Class
