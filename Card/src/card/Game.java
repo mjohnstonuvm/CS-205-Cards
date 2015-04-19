28c6adeb30b16deb1c85f020d54b7e8c2d4cfdbb
Matt <matt.johnston@uvm.edu>
2015-04-19 15:18:13 -0400

    fixed errors

--- Card/src/card/Game.java
+++ Card/src/card/Game.java
@@ -1,4 +1,4 @@
-//package card;
+package card;
 
 import java.util.ArrayList;
 
@@ -23,7 +23,7 @@ public class Game extends CardObject {
         //sets the playerName
         this.playerName = playerName;
         //sets the easy ai
-        ai = new EasyAI(numOfAI+1);
+        ai = new EasyAI(numOfAI);
 
         //creates the user hand
         hands.add(new Hand(hands.get(0).peek(0), hands.get(0).peek(1), hands.get(0).peek(2), hands.get(0).peek(3)));
@@ -60,7 +60,7 @@ public class Game extends CardObject {
         //end game options can go in the while loop
         while (gameState && roundCount < 10) {
             System.out.println("Round # : " + roundCount);
-            for (int i = 0; i < hands.size(); i++) {
+            for (int i = 0; i < data.hands.size(); i++) {
                 // Player's Turn
                 if (i == 0) {
                     gameState = false;
@@ -81,18 +81,18 @@ public class Game extends CardObject {
     
     protected void opponentTurn(int oppNum) {
         boolean drawDecision;
-        Hand oppHand = data.hand.get(oppNum);
+        Hand oppHand = data.hands.get(oppNum);
         int[] result;
-        String fileName;
+        String fileName = "";
         //if discard pile is not empty
-        Card selectedCard = dp.draw(); //draws from discard pile
+        Card selectedCard = data.dp.draw(); //draws from discard pile
         drawDecision = ai.DrawOrDiscard(selectedCard); //returns action
         
         if (drawDecision) {
-            Card selectedCard = deck.pop();
-            while(selectedCard == Type.DRAW2) {
+            selectedCard = data.deck.pop();
+            while(selectedCard.getType() == Type.DRAW2) {
                 data.dp.add(selectedCard);
-                selectedCard = deck.pop();
+                selectedCard = data.deck.pop();
             }
             result = ai.CardDraw(selectedCard);
         }
@@ -104,31 +104,31 @@ public class Game extends CardObject {
         // produce fileNames to send to gui
         // result[0] == 0: when the AI wants to discard the card that was drawn
         // result[0] == 2: when the AI wants uses a peek card, effectly does nothing
-        if (result[0] == 0 || result == 2) {
+        if (result[0] == 0 || result[0] == 2) {
             data.dp.add(selectedCard);
-            fileName == selectedCard.getCard();
+            fileName = selectedCard.getCard();
         }
         // result[0] == 1: when the AI wants to exchange a card in its hand with the one drawn
         else if (result[0] == 1) {
             Card toDiscard = oppHand.swap(selectedCard, result[1]);
             data.dp.add(toDiscard);
-            fileName == toDiscard.getCard();
+            fileName = toDiscard.getCard();
         }
         // result[0] == 3: when the AI uses a swap card to swap cards with another player
         else if (result[0] == 3) {
             // Swap cards between opponent and other player
             Hand otherHand = data.hands.get(result[2]);
-            Card otherCard = otherHand.swap(oppHand.get(result[1]), result[3])
-            otherCard = oppHand.swap(otherHand, result[1])
+            Card otherCard = otherHand.swap(oppHand.peek(result[1]), result[3]);
+            otherCard = oppHand.swap(otherHand.peek(result[3]), result[1]);
             otherCard = null;
             // Update the hand that the opponent swapped cards with
             data.hands.set(result[2], otherHand);
-            fileName == selectedCard.getCard();
+            fileName = selectedCard.getCard();
         }
         // else, say an error occurred
         else {
-            System.out.println("Game.java Error: AI result not in the correct format. 
-                result[0] should be an int from 0 to 3");
+            System.out.println("Game.java Error: AI result not in the correct format "
+                    + "result[0] should be an int from 0 to 3");
         }
         // Update opponent hand after turn is complete
         data.hands.set(oppNum, oppHand);
