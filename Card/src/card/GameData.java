b31e208623bdf4198539c90b925dc21df5d30bb8
Matt <matt.johnston@uvm.edu>
2015-04-20 18:55:54 -0400

    int net beans

--- Card/src/card/GameData.java
+++ Card/src/card/GameData.java
@@ -1,18 +1,26 @@
+/*
+Created by Keegan and Matt
+*/
+package card;
+
+import java.util.ArrayList;
+
 public class GameData {
 	
 	public Deck deck;
 	public DiscardPile dp;
 	public ArrayList<Hand> hands;
 	public String playerName;
+        public int numOfAI;
 	public boolean firstRound;
 
-	public GameData(Deck deck, DiscardPile dp, ArrayList<Hand> hands, String playerName) {
+	public GameData(Deck deck, DiscardPile dp, ArrayList<Hand> hands, String playerName,int numOfAI) {
 		this.deck = deck;
 		this.dp = dp;
 		this.hands = hands;
 		this.playerName = playerName;
+                this.numOfAI = numOfAI;
 		firstRound = true;
 	}// End of Constructor
-
 
 }// End of GameData
